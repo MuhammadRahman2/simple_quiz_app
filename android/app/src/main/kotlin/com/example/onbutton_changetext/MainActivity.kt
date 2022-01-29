5e7d44036f4f16a5793d46fa64b5865a3a293d4e
MuhammadRahman2 <Rahman@2042>
2022-01-29 14:06:25 +0500

    simple quiz app

---
+++ android/app/src/main/kotlin/com/example/onbutton_changetext/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.onbutton_changetext
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/answer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10,left: 10,right: 10),
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
